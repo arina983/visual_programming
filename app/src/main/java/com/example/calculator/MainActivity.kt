b6e28081e01129aa686437c702f889e6a9dd33f0
arina983 <arina.bubenina@mail.ru>
2025-10-11 15:02:41 +0700

    Исправлена логика калькулятора

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -13,6 +13,7 @@ class MainActivity : AppCompatActivity() {
     private var firstNumber = 0.0
     private var currentOperator = ""
 
+    private var nextOperator =  false
     private lateinit var resultText: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,97 +29,168 @@ class MainActivity : AppCompatActivity() {
 
         val button0 = findViewById<Button>(R.id.button0)
         button0.setOnClickListener{
-            currentNumber += "0"
-            resultText.text = currentNumber
+            if (nextOperator) {
+                currentNumber = "0"
+                nextOperator = false
+                resultText.text = currentNumber
+            } else {
+                currentNumber += "0"
+                resultText.text = currentNumber
+            }
         }
         val button1 = findViewById<Button>(R.id.button1)
         button1.setOnClickListener{
-            currentNumber += "1"
-            resultText.text = currentNumber
+            if (nextOperator) {
+                currentNumber = "1"
+                nextOperator = false
+                resultText.text = currentNumber
+            } else {
+                currentNumber += "1"
+                resultText.text = currentNumber
+            }
         }
         val button2 = findViewById<Button>(R.id.button2)
         button2.setOnClickListener{
-            currentNumber += "2"
-            resultText.text = currentNumber
+            if (nextOperator) {
+                currentNumber = "2"
+                nextOperator = false
+                resultText.text = currentNumber
+            } else {
+                currentNumber += "2"
+                resultText.text = currentNumber
+            }
         }
         val button3 = findViewById<Button>(R.id.button3)
         button3.setOnClickListener{
-            currentNumber += "3"
-            resultText.text = currentNumber
+            if (nextOperator) {
+                currentNumber = "3"
+                nextOperator = false
+                resultText.text = currentNumber
+            } else {
+                currentNumber += "3"
+                resultText.text = currentNumber
+            }
         }
         val button4 = findViewById<Button>(R.id.button4)
         button4.setOnClickListener{
-            currentNumber += "4"
-            resultText.text = currentNumber
+            if (nextOperator) {
+                currentNumber = "4"
+                nextOperator = false
+                resultText.text = currentNumber
+            } else {
+                currentNumber += "4"
+                resultText.text = currentNumber
+            }
         }
         val button5 = findViewById<Button>(R.id.button5)
         button5.setOnClickListener{
-            currentNumber += "5"
-            resultText.text = currentNumber
+            if (nextOperator) {
+                currentNumber = "5"
+                nextOperator = false
+                resultText.text = currentNumber
+            } else {
+                currentNumber += "5"
+                resultText.text = currentNumber
+            }
         }
         val button6 = findViewById<Button>(R.id.button6)
         button6.setOnClickListener{
-            currentNumber += "6"
-            resultText.text = currentNumber
+            if (nextOperator) {
+                currentNumber = "6"
+                nextOperator = false
+                resultText.text = currentNumber
+            } else {
+                currentNumber += "6"
+                resultText.text = currentNumber
+            }
         }
         val button7 = findViewById<Button>(R.id.button7)
         button7.setOnClickListener{
-            currentNumber += "7"
-            resultText.text = currentNumber
+            if (nextOperator) {
+                currentNumber = "7"
+                nextOperator = false
+                resultText.text = currentNumber
+            } else {
+                currentNumber += "7"
+                resultText.text = currentNumber
+            }
         }
         val button8 = findViewById<Button>(R.id.button8)
         button8.setOnClickListener{
-            currentNumber += "8"
-            resultText.text = currentNumber
+            if (nextOperator) {
+                currentNumber = "8"
+                nextOperator = false
+                resultText.text = currentNumber
+            } else {
+                currentNumber += "8"
+                resultText.text = currentNumber
+            }
         }
         val button9 = findViewById<Button>(R.id.button9)
         button9.setOnClickListener{
-            currentNumber += "9"
-            resultText.text = currentNumber
+            if (nextOperator) {
+                currentNumber = "9"
+                nextOperator = false
+                resultText.text = currentNumber
+            } else {
+                currentNumber += "9"
+                resultText.text = currentNumber
+            }
         }
         val buttonPoint = findViewById<Button>(R.id.buttonPoint)
-        buttonPoint.setOnClickListener{
-            currentNumber += "."
-            resultText.text = currentNumber
+        buttonPoint.setOnClickListener {
+            if (nextOperator) {
+                currentOperator = "."
+            } else {
+                currentNumber += "."
+                resultText.text = currentNumber
+            }
         }
         val buttonPlus = findViewById<Button>(R.id.buttonPlus)
         buttonPlus.setOnClickListener{
-            firstNumber = currentNumber.toDouble()
-            currentOperator = "+"
-            currentNumber = ""
+            if (nextOperator){
+                currentOperator = "+"
+            } else {
+                firstNumber = currentNumber.toDouble()
+                currentOperator = "+"
+                currentNumber = ""
+                nextOperator = true
+            }
         }
         val buttonMinus = findViewById<Button>(R.id.buttonMinus)
         buttonMinus.setOnClickListener{
-            firstNumber = currentNumber.toDouble()
-            currentOperator = "-"
-            currentNumber = ""
+            if (nextOperator){
+                currentOperator = "-"
+            } else {
+                firstNumber = currentNumber.toDouble()
+                currentOperator = "-"
+                currentNumber = ""
+                nextOperator = true
+            }
         }
         val buttonMultiply = findViewById<Button>(R.id.buttonMultiply)
         buttonMultiply.setOnClickListener{
-            firstNumber = currentNumber.toDouble()
-            currentOperator = "×"
-            currentNumber = ""
+            if (nextOperator){
+                currentOperator = "×"
+            } else {
+                firstNumber = currentNumber.toDouble()
+                currentOperator = "×"
+                currentNumber = ""
+                nextOperator = true
+            }
         }
         val buttonDivision = findViewById<Button>(R.id.buttonDivision)
         buttonDivision.setOnClickListener{
-            firstNumber = currentNumber.toDouble()
-            currentOperator = "÷"
-            currentNumber = ""
-        }
-        val buttonEquals = findViewById<Button>(R.id.buttonEquals)
-        buttonEquals.setOnClickListener {
-           val secondNumber = currentNumber.toDouble()
-           var result = 0.0
-            when(currentOperator) {
-                "+" -> result = firstNumber + secondNumber
-                "-" -> result = firstNumber - secondNumber
-                "×" -> result = firstNumber * secondNumber
-                "÷" -> result = firstNumber / secondNumber
+            if (nextOperator){
+                currentOperator = "÷"
+            } else {
+                firstNumber = currentNumber.toDouble()
+                currentOperator = "÷"
+                currentNumber = ""
+                nextOperator = true
             }
-            resultText.text = result.toString()
-            currentNumber = result.toString()
-
         }
+
         val buttonC = findViewById<Button>(R.id.buttonC)
         buttonC.setOnClickListener {
             currentNumber = ""
@@ -126,6 +198,24 @@ class MainActivity : AppCompatActivity() {
             currentOperator = ""
             resultText.text = "0"
 
+        }
+        val buttonEquals = findViewById<Button>(R.id.buttonEquals)
+        buttonEquals.setOnClickListener {
+            val secondNumber = currentNumber.toDouble()
+            var result = 0.0
+            when (currentOperator) {
+                "+" -> result = firstNumber + secondNumber
+                "-" -> result = firstNumber - secondNumber
+                "×" -> result = firstNumber * secondNumber
+                "÷" -> result = firstNumber / secondNumber
+            }
+            if (result % 1 == 0.0) {
+                resultText.text = result.toInt().toString()
+                currentNumber = result.toInt().toString()
+            } else {
+                resultText.text = result.toString()
+                currentNumber = result.toString()
+            }
         }
     }
 }
